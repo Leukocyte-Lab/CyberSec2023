ea4db1d8cee52eba22a10aebb9acb9d7755b4152
aries0d0f <aries0d0f@gmail.com>
2023-04-26 13:57:21 +0800

    build: Config font plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 import Vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
+import Unfonts from 'unplugin-fonts';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -25,6 +26,18 @@ export default defineConfig({
       dirs: ['src/components'],
       include: [/\.vue$/, /\.vue\?vue/]
     }),
+    Unfonts.vite({
+      google: {
+        preconnect: true,
+        display: 'swap',
+        families: [
+          {
+            name: 'Noto Sans TC',
+            styles: 'wght@400;500;700;900'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
