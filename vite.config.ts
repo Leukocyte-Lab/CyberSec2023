596f2f221133466ef79fbe12ee91b008014db54f
aries0d0f <aries0d0f@gmail.com>
2023-04-26 13:59:26 +0800

    build: Config icon plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import Unfonts from 'unplugin-fonts';
+import Icons from 'unplugin-icons/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -25,6 +26,9 @@ export default defineConfig({
       deep: true,
       dirs: ['src/components'],
       include: [/\.vue$/, /\.vue\?vue/]
+    }),
+    Icons({
+      autoInstall: true,
     }),
     Unfonts.vite({
       google: {
