55b7bd07e082c848aa5ce2ed93b246dccf6b1637
aries0d0f <aries0d0f@gmail.com>
2023-05-02 11:41:45 +0800

    chore: Track auto-import declarations.

--- components.d.ts
+++ components.d.ts
@@ -9,10 +9,17 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AGHEyeRingLogo: typeof import('./src/components/Element/AGHEyeRingLogo.vue')['default']
+    AGHLogo: typeof import('./src/components/Element/AGHLogo.vue')['default']
     CompanyLogo: typeof import('./src/components/Element/CompanyLogo.vue')['default']
     DashLine: typeof import('./src/components/Element/DashLine.vue')['default']
     EventLogo: typeof import('./src/components/Element/EventLogo.vue')['default']
+    IconIcClock: typeof import('~icons/ic/clock')['default']
+    IconIcRoundAccessTimeFilled: typeof import('~icons/ic/round-access-time-filled')['default']
     IconIcRoundClose: typeof import('~icons/ic/round-close')['default']
+    IconIcRoundLocationOn: typeof import('~icons/ic/round-location-on')['default']
+    IconIcTime: typeof import('~icons/ic/time')['default']
+    IconMdiTime: typeof import('~icons/mdi/time')['default']
     MainBanner: typeof import('./src/components/Layout/MainBanner.vue')['default']
     MainContent: typeof import('./src/components/Layout/MainContent.vue')['default']
     OutlineTextGradient: typeof import('./src/components/Effects/OutlineTextGradient.vue')['default']
