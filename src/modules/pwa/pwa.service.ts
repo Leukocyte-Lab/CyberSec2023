3f02d4139406238dc870c180437ea84dca0abca7
aries0d0f <aries0d0f@gmail.com>
2023-05-02 16:26:36 +0800

    fix(modules): Skip service worker register if not client.

--- src/modules/pwa/pwa.service.ts
+++ src/modules/pwa/pwa.service.ts
@@ -2,21 +2,27 @@
 
 import type { InjectionKey } from 'vue';
 
+import { useCtx } from '@/modules/utils/context/context.service';
 import { createModuleSetup } from '@/modules/utils/module-factory';
 
 const PROVIDE_KEY: InjectionKey<void> = Symbol('PwaRegister');
 
 const $useRegisterSW = async () => {
+  const { isClient } = useCtx();
+  if (!isClient) {
+    return false;
+  }
+
   const { registerSW } = await import('virtual:pwa-register');
-    registerSW({
-      onRegistered(r) {
-        if (!r) return;
+  registerSW({
+    onRegistered(r) {
+      if (!r) return;
+      r.update();
+      setInterval(() => {
         r.update();
-        setInterval(() => {
-          r.update();
-        }, 60 * 60 * 1000);
-      }
-    });
+      }, 60 * 60 * 1000);
+    }
+  });
 };
 
 export const setup = createModuleSetup(PROVIDE_KEY, $useRegisterSW);
