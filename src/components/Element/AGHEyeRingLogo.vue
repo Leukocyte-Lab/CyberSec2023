e8d911537578be17281f706887a8d3687ff08103
aries0d0f <aries0d0f@gmail.com>
2023-05-02 11:34:34 +0800

    feat(view): Add AGH logo elements as component.

---
+++ src/components/Element/AGHEyeRingLogo.vue
@@ -0,0 +1,173 @@
+<template>
+  <svg
+    viewBox="0 0 288 294"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="logo"
+  >
+    <g filter="url(#filter0_d_222_1021)">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M140.021 54.2813C142.485 52.8683 145.515 52.8683 147.979 54.2813L214.979 92.6947C217.466 94.1206 219 96.7682 219 99.6349V176.365C219 179.232 217.466 181.879 214.979 183.305L147.979 221.719C145.515 223.132 142.485 223.132 140.021 221.719L73.0209 183.305C70.5339 181.879 69 179.232 69 176.365L69 99.6349C69 96.7682 70.5339 94.1205 73.0209 92.6947L140.021 54.2813Z"
+        stroke="url(#paint0_linear_222_1021)"
+        stroke-width="2"
+        shape-rendering="crispEdges"
+      />
+    </g>
+    <path
+      d="M137.79 192.587L102.095 171.863L92 166C93.2674 168.748 95.3613 171.087 98.0392 172.649L136.435 194.947C137.151 195.368 137.9 195.712 138.672 196L142 194.072C140.534 193.839 139.113 193.351 137.801 192.587H137.79Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M189.56 170.87L150.519 192.739C150.519 192.739 150.485 192.75 150.474 192.761L143 196.946C143.448 196.978 143.885 197 144.333 197C146.944 197 149.567 196.348 151.897 195.043L190.938 173.174C191.667 172.761 192.361 172.293 193 171.783V168C192.059 169.141 190.905 170.12 189.56 170.87Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M194.947 116.356V160.497C194.947 160.497 194.947 160.519 194.947 160.53V169C196.903 166.542 198 163.58 198 160.497V116.356C198 115.533 197.925 114.71 197.763 113.909L194 112C194.598 113.372 194.935 114.853 194.935 116.356H194.947Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M189.959 103.351L151.556 81.0528C150.839 80.6317 150.09 80.2881 149.329 80L146 81.9283C147.455 82.1611 148.888 82.6487 150.2 83.4134L188.604 105.711C188.604 105.711 188.604 105.711 188.615 105.711L196 110C194.732 107.252 192.638 104.913 189.959 103.351Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M137.103 80.9491L98.0619 102.825C97.3335 103.238 96.6387 103.694 96 104.216V108C96.9413 106.869 98.0955 105.891 99.429 105.141L138.47 83.2649C138.47 83.2649 138.548 83.2323 138.582 83.2106L146 79.0572C142.952 78.7963 139.826 79.4269 137.103 80.9599V80.9491Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M90 116.513V160.645C90 161.468 90.0748 162.29 90.2368 163.091L94 165C93.4019 163.629 93.0654 162.148 93.0654 160.645V116.513C93.0654 116.513 93.0654 116.436 93.0654 116.392V108C91.109 110.457 90 113.419 90 116.502V116.513Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M180.151 135.153L148.499 86.4311C146.391 83.1896 141.598 83.1896 139.501 86.4311L107.849 135.153C106.717 136.894 106.717 139.117 107.849 140.847L139.501 189.569C141.609 192.81 146.402 192.81 148.499 189.569L180.151 140.847C181.283 139.106 181.283 136.883 180.151 135.153ZM144.006 109.779C156.964 109.779 167.925 118.332 171.42 130.017L167.77 127.936C163.809 118.847 154.645 112.462 144.006 112.462C133.366 112.462 124.202 118.847 120.241 127.936L116.591 130.017C120.086 118.332 131.058 109.779 144.006 109.779ZM144.006 166.21C131.047 166.21 120.086 157.657 116.591 145.983L120.241 148.064C124.202 157.154 133.366 163.527 144.006 163.527C154.645 163.527 163.809 157.154 167.77 148.064L171.42 145.983C167.925 157.668 156.953 166.21 144.006 166.21ZM173.861 141.493L146.047 157.34C144.782 158.062 143.218 158.062 141.964 157.34L114.15 141.493C112.875 140.76 112.109 139.457 112.109 138C112.109 136.544 112.875 135.24 114.15 134.507L141.964 118.649C142.597 118.288 143.296 118.113 144.006 118.113C144.716 118.113 145.415 118.288 146.047 118.649L173.861 134.507C175.137 135.24 175.902 136.544 175.902 138C175.902 139.457 175.137 140.76 173.861 141.493Z"
+      fill="#BDBDBD"
+    />
+    <path
+      d="M143.5 152C150.956 152 157 145.956 157 138.5C157 131.044 150.956 125 143.5 125C136.044 125 130 131.044 130 138.5C130 145.956 136.044 152 143.5 152Z"
+      fill="#BDBDBD"
+    />
+    <text
+      fill="white"
+      xml:space="preserve"
+      style="white-space: pre"
+      font-family="Noto Sans TC"
+      font-size="12"
+      font-weight="bold"
+      letter-spacing="0em"
+    >
+      <tspan x="0" y="92.732">&#x99ed;&#x5ba2;&#x5165;&#x4fb5;&#10;</tspan>
+      <tspan x="0" y="109.732">&#x7b56;&#x7565;&#x6f14;&#x7df4;</tspan>
+    </text>
+    <text
+      fill="white"
+      xml:space="preserve"
+      style="white-space: pre"
+      font-family="Noto Sans TC"
+      font-size="12"
+      font-weight="bold"
+      letter-spacing="0em"
+    >
+      <tspan x="0" y="185.732">&#x8cc7;&#x5b89;&#x9632;&#x8b77;&#10;</tspan>
+      <tspan x="0" y="202.732">&#x5f37;&#x97cc;&#x6027;&#x91cf;&#x6e2c;</tspan>
+    </text>
+    <text
+      fill="white"
+      xml:space="preserve"
+      style="white-space: pre"
+      font-family="Noto Sans TC"
+      font-size="12"
+      font-weight="bold"
+      letter-spacing="0em"
+    >
+      <tspan x="120" y="253.732">&#x8cc7;&#x5b89;&#x9632;&#x8b77;&#10;</tspan>
+      <tspan x="114" y="270.732">&#x6709;&#x6548;&#x6027;&#x8a55;&#x4f30;</tspan>
+    </text>
+    <text
+      fill="white"
+      xml:space="preserve"
+      style="white-space: pre"
+      font-family="Noto Sans TC"
+      font-size="12"
+      font-weight="bold"
+      letter-spacing="0em"
+    >
+      <tspan x="240" y="184.732">&#x8cc7;&#x5b89;&#x6295;&#x8cc7;&#10;</tspan>
+      <tspan x="240" y="201.732">&#x6548;&#x76ca;&#x8a55;&#x4f30;</tspan>
+    </text>
+    <text
+      fill="white"
+      xml:space="preserve"
+      style="white-space: pre"
+      font-family="Noto Sans TC"
+      font-size="12"
+      font-weight="bold"
+      letter-spacing="0em"
+    >
+      <tspan x="240" y="93.732">&#x6c7a;&#x7b56;&#x63a8;&#x6f14;&#10;</tspan>
+      <tspan x="240" y="110.732">&#x81e8;&#x5834;&#x6a21;&#x64ec;</tspan>
+    </text>
+    <text
+      fill="white"
+      xml:space="preserve"
+      style="white-space: pre"
+      font-family="Noto Sans TC"
+      font-size="12"
+      font-weight="bold"
+      letter-spacing="0em"
+    >
+      <tspan x="108" y="13.732">&#x8cc7;&#x5b89;&#x89e3;&#x6c7a;&#x65b9;&#x6848;&#10;</tspan>
+      <tspan x="102.557" y="30.732">&#x6982;&#x5ff5;&#x9a57;&#x8b49; (PoC)</tspan>
+    </text>
+    <defs>
+      <filter
+        id="filter0_d_222_1021"
+        x="8"
+        y="2.22156"
+        width="272"
+        height="291.557"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="10" />
+        <feGaussianBlur stdDeviation="30" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0.5116 0 0 0 0 0.45 0 0 0 0 1 0 0 0 1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_222_1021" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_222_1021"
+          result="shape"
+        />
+      </filter>
+      <linearGradient
+        id="paint0_linear_222_1021"
+        x1="159.938"
+        y1="224"
+        x2="229.577"
+        y2="87.539"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#C59EF3" />
+        <stop offset="0.351841" stop-color="#A277FD" />
+        <stop offset="0.666667" stop-color="#7763F1" />
+        <stop offset="1" stop-color="#6C59FF" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<style scoped>
+.logo {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
